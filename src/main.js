3e1424128e2c4168bd0b6e70f32701bdcf003320
Andreas Graner <andreasgraner@Andreass-MBP.local>
2019-09-01 16:40:01 +0200

    added VueMq

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueMq from "vue-mq"
 
 Vue.config.productionTip = false;
 
@@ -10,3 +11,11 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+Vue.use(VueMq, {
+  breakpoints: {
+    sm: 450,
+    md: 1250,
+    lg: Infinity
+  }
+})
