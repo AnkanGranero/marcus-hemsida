3e33afa4d8485f9bdfc222dde77d771841cb53da
Andreas Graner <andreasgraner@Andreass-MBP.local>
2019-09-14 15:07:56 +0200

    v-slot

--- src/views/Coaching.vue
+++ src/views/Coaching.vue
@@ -1,46 +1,42 @@
 <template>
   <div class="Coaching">
-
-<PageBody :propName="name">
- <h2>De generella grunderna för ett coachsamtal:</h2> 
- <ul>
-   <li>1. Du definierar målet för samtalet, det kan vara en handlingsplan eller en insikt du vill uppnå.
-</li>
-   <li>2. Coachingen tydliggör vad som är viktigt, undanröjer hinder på vägen och plockar upp lärdomar på vägen.
-</li>
-   <li>3. Du och jag kliver in i ett partnerskap där vi utforskar, utmanar, ser möjligheter, sorterar och fokuserar på det viktiga i samtalet.
-</li>
- </ul>
- <div class="wrapper">
-   <img src="" alt="">
-   <p>Jag arbetar med utgångspunkten att du är experten i ditt liv. 
-     Jag hjälper dig att hitta lösningar på problem och gör det som är bra, ännu bättre genom frågor och verktyg. 
-     Jag hjälper dig att se klarare i ditt beslutsfattande.</p>
- </div>
- <p>Förstå vad coaching innebär och boka ett förutsättningslöst möte med mig på 20 min.</p>
-</PageBody>
-
+    <PageBody :propName="name">
+      <template v-slot:body-text>
+        <h2>De generella grunderna för ett coachsamtal:</h2>
+        <ul>
+          <li>1. Du definierar målet för samtalet, det kan vara en handlingsplan eller en insikt du vill uppnå.</li>
+          <li>2. Coachingen tydliggör vad som är viktigt, undanröjer hinder på vägen och plockar upp lärdomar på vägen.</li>
+          <li>3. Du och jag kliver in i ett partnerskap där vi utforskar, utmanar, ser möjligheter, sorterar och fokuserar på det viktiga i samtalet.</li>
+        </ul>
+        <div class="wrapper">
+          <img src alt />
+          <p>
+            Jag arbetar med utgångspunkten att du är experten i ditt liv.
+            Jag hjälper dig att hitta lösningar på problem och gör det som är bra, ännu bättre genom frågor och verktyg.
+            Jag hjälper dig att se klarare i ditt beslutsfattande.
+          </p>
+        </div>
+        <p>Förstå vad coaching innebär och boka ett förutsättningslöst möte med mig på 20 min.</p>
+      </template>
+    </PageBody>
   </div>
 </template>
 
 <script>
-import PageBody from "@/components/PageBody.vue"
+import PageBody from "@/components/PageBody.vue";
 
 export default {
   name: "Coaching",
   data() {
-        return { name: "Coaching",
-         }
+    return { name: "Coaching" };
   },
   components: {
     PageBody
-
   }
 };
 </script>
 <style lang="scss">
 .wrapper {
-  display:flex;
+  display: flex;
 }
-
 </style>
--- src/views/Kontakt.vue
+++ src/views/Kontakt.vue
@@ -1,41 +1,40 @@
 <template>
   <div class="Kontakt">
-
-<PageBody :propName="name">
-
-<p>Jag heter Marcus von Eckermann</p>
-<p>Jag är modig, rak och nyfiken. 
-  Jag har förmågan att bygga relation snabbt med lekfullhet som får andra att våga testa sig fram. Jag är avslappnad med ett leende som smittar. 
-  Jag har lätt att förstå andra och föder kreativitet i samtalet.</p>
-  <p>Jag är en intuitiv/empatisk coach i första hand med förmågan till god analytisk förmåga. 
-    Jag är helt enkelt skicklig på att se helheten och höra vad det egentligen handlar om. Det kan vara skönt för vissa och lite läskigt för andra. Du ska dock veta att jag är ytterst respektfull i mitt sätt att föra mig i samtalet med dig och följer ICF:s etiska riktlinjer och kärnkompetenserna. 
-    Jag kan berätta mer om detta i ett introducerande samtal.</p>
-    <h2>Kvalifikationer</h2>
-    <ul>
-      <li>1. Jag är diplomerad coach hos Leapfrog AB</li>
-      <li>2. Utbildad i självledarskap och personlig utveckling</li>
-      <li>3. Erfarenhet som chef</li>
-      <li>4. Mitt eget verktyg: Typologi</li>
-       </ul>
-      </PageBody>
-      
-
-
-   
+    <PageBody :propName="name">
+      <template v-slot:body-text>
+        <p>Jag heter Marcus von Eckermann</p>
+        <p>
+          Jag är modig, rak och nyfiken.
+          Jag har förmågan att bygga relation snabbt med lekfullhet som får andra att våga testa sig fram. Jag är avslappnad med ett leende som smittar.
+          Jag har lätt att förstå andra och föder kreativitet i samtalet.
+        </p>
+        <p>
+          Jag är en intuitiv/empatisk coach i första hand med förmågan till god analytisk förmåga.
+          Jag är helt enkelt skicklig på att se helheten och höra vad det egentligen handlar om. Det kan vara skönt för vissa och lite läskigt för andra. Du ska dock veta att jag är ytterst respektfull i mitt sätt att föra mig i samtalet med dig och följer ICF:s etiska riktlinjer och kärnkompetenserna.
+          Jag kan berätta mer om detta i ett introducerande samtal.
+        </p>
+        <h2>Kvalifikationer</h2>
+        <ul>
+          <li>1. Jag är diplomerad coach hos Leapfrog AB</li>
+          <li>2. Utbildad i självledarskap och personlig utveckling</li>
+          <li>3. Erfarenhet som chef</li>
+          <li>4. Mitt eget verktyg: Typologi</li>
+        </ul>
+      </template>
+    </PageBody>
   </div>
 </template>
 
 <script>
-import PageBody from "@/components/PageBody.vue"
+import PageBody from "@/components/PageBody.vue";
 
 export default {
   name: "Kontakt",
   data() {
-        return { name: "Kontakt" }
+    return { name: "Kontakt" };
   },
   components: {
     PageBody
-
   }
 };
 </script>
