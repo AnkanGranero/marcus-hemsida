c380690b61559e7bca7089b66d07cae5e8efd1b9
Andreas Graner <andreasgraner@Andreass-MBP.local>
2019-09-14 15:05:44 +0200

    fixed styling

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
   <div id="app">
-
-     <Header-component class="desktop-header"></Header-component>
+    <Header-component class="desktop-header"></Header-component>
 
     <router-view />
   </div>
 </template>
 
 <script>
-import HeaderComponent from "./components/HeaderComponent"
+import HeaderComponent from "./components/HeaderComponent";
 export default {
   name: "App",
   components: { HeaderComponent }
-
-}
+};
 </script>
 
 <style lang="scss">
-
-
 #app {
-  font-family: "ambroise-std-1","ambroise-std-2",serif;
-  
-  }
-
+  font-family: sans-serif;
+  overflow: hidden;
+}
 
 #nav a.router-link-exact-active {
   color: #42b983;
@@ -33,8 +28,12 @@ li {
   list-style: none;
 }
 
-a{
+a {
   text-decoration: none;
-  color: black
+  color: black;
+}
+
+p {
+  font-weight: 100;
 }
 </style>
