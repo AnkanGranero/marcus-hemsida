dda59375b3d2945fca435e82a9a3d4f96a2ab1c8
Andreas Graner <andreasgraner@Andreass-MBP.local>
2019-09-14 15:04:56 +0200

    added picture

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="home">
-
-<PageBody :propName="name"></PageBody>
+    <PageBody :propName="name"></PageBody>
   </div>
 </template>
 
 <script>
-import PageBody from "@/components/PageBody.vue"
-
+import PageBody from "@/components/PageBody.vue";
 export default {
   name: "home",
   data() {
     return {
       name: "Home"
-    }
+    };
   },
   components: {
     PageBody
-
   }
 };
 </script>
