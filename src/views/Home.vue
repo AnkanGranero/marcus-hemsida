061b2f16f7bb65e09b3eb7e4b87682717c029335
Andreas Graner <grannemannen@gmail.com>
2019-10-10 20:39:35 +0200

    home text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
     <Hero :propName="name"></Hero>
+    <PageBody :propName="name">
+      <template v-slot:box-1>
+        <h2>Lorem ipsum</h2>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          <!--     </p>
+          <p>-->
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+      </template>
+    </PageBody>
     <Footer />
   </div>
 </template>
@@ -24,3 +37,11 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+@import "@/scss/_variables.scss";
+@import "@/scss/_colors.scss";
+@import "@/scss/_sizes.scss";
+.welcome {
+  background: $dark;
+}
+</style>
