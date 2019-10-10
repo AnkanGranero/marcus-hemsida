f415b8b7132b24406cce5f1b6629d9d09e2bcb4b
Andreas Graner <grannemannen@gmail.com>
2019-10-10 20:35:26 +0200

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ cd dist
 git init
 git add -A 
 git commit -m "deploy"
+
 git push -f git@github.com:ankangranero/marcus-hemsida.git master:gh-pages
 cd -
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicPath: "/marcus-hemsida/",
+/*     css: {
+      loaderOptions: {
+        sass: {
+          data: `@import "@/styles/_variables.scss";`
+        }
+      }
+    } */
+  };
